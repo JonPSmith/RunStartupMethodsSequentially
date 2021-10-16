63a9295a0e3a08cb6121d9b136f02998dae20b3c
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-10-16 10:41:48 +0100

    Add project files.

---
+++ RunStartupMethodsSequentially/RunSequentiallyOptions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RunStartupMethodsSequentially
+{
+    public class RunSequentiallyOptions
+    {
+
+        public string LockDatabaseName { get; set; } = nameof(LockDatabaseAndRunJob);
+
+        public int DefaultLockTimeoutInSeconds { get; set; } = 100;
+
+        public RunSequentiallyOptions(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; }
+
+        public ICollection<TryLockVersion> LockVersionsInOrder { get; } = new List<TryLockVersion>();
+    }
+}
---
+++ RunStartupMethodsSequentially/StartupExtentions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RunStartupMethodsSequentially
+{
+    public static class StartupExtensions
+    {
+        public static RunSequentiallyOptions RegisterRunMethodsSequentially(this IServiceCollection services,
+            Action<RunSequentiallyOptions> options = null)
+        {
+            var lockOptions = new RunSequentiallyOptions(services);
+            options?.Invoke(lockOptions);
+
+
+            return lockOptions;
+        }
+    }
+}
